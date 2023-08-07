aa854ba27775925b56d22b33b68f670bb89607e1
wangqiangqiang <ilove1314>
2023-08-07 16:49:57 +0800

    优化上传

--- src/test/java/CmsHandler.java
+++ src/test/java/CmsHandler.java
@@ -1,24 +1,18 @@
 import com.villa.cms.DBHandler;
 
-import java.net.URLDecoder;
-
-/**
- * @作者 微笑い一刀
- * @bbs_url https://blog.csdn.net/u012169821
- */
 public class CmsHandler {
     public static void main(String[] args) {
-            DBHandler handler = new DBHandler("jdbc:mysql://120.26.45.47:3306/tg?useUnicode=true&characterEncoding=utf8&allowMultiQueries=true&serverTimezone=GMT%2B8&useSSL=false",
-                    "tg", "Dhnf7kfENdHSJwnr");
-//            handler.setPackageName("com.villa.user");
-//            /**
-//             * 生成某一张表的全部
-//             */
-//            handler.autoOneByTableName("user");
-            /**
-             * 生成某一张表的基础文件 基础模型和基础dao
-             */
-//        handler.createBaseModelAndBaseMapper("user");
+        DBHandler handler = new DBHandler("jdbc:mysql:///xx?useUnicode=true&characterEncoding=utf8&allowMultiQueries=true&serverTimezone=GMT%2B8&useSSL=false",
+                "root", "123456");
+        //设置基础包 默认com.villa
+        handler.setPackageName("com.villa");
+        //生成某一张表的全部
+        handler.autoOneByTableName("user");
+        //生成某一张表的父级模型 父级dao/xml 自定义内容写在子类或子xml中，不会被此方法覆盖
+        handler.createBaseModelAndBaseMapper("user");
+        //生成某个表的后台CRUD 需要集合模板项目使用
         handler.createVue("product");
+        //生成所有表的所有后台资源 从模型到控制器 不包含vue/js文件
+        handler.auto();
     }
 }
